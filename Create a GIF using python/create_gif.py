3ecbead070d5e009155f09b301dd81fab7de11d4
Ankush Khakale <khakaleankush@gmail.com>
2025-11-13 01:32:23 +0530

    Started work on the Project- GIF using the Python

---
+++ Create a GIF using python/create_gif.py
@@ -0,0 +1,6 @@
+# Imageio is a Python library that provides an easy interface to read and write a wide range of image data. It runs on Python 3.5 and above.
+
+import imageio.v3 as iio
+
+filenames = ['codedex1.jpg', 'codedex2.jpg'] # List of image files to include in the GIF
+images = [] # List to hold the image data
